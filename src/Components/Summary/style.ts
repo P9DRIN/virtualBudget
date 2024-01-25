f4c68ab8094258aa10e9b9ae58b7bb5ba191b094
Pedro Henrique Lima Silva <121348100+P9DRIN@users.noreply.github.com>
2024-01-25 20:52:14 -0300

    Update style.ts

--- src/Components/Summary/style.ts
+++ src/Components/Summary/style.ts
@@ -1,11 +1,37 @@
 import styled, { css } from 'styled-components'
 
 export const Container = styled.div`
+  
+  @media(max-width:425px){
+    display: flex;
+    justify-content: center;
+  }
+  @media(min-width: 426px){
+    display: flex;
+    justify-content: center;
+  }
+    
+`
+export const CardContainer = styled.div`
+   @media(max-width: 425px){
+    display: flex;
+    flex-wrap: wrap;
+
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;    
+
+    margin-bottom: 1rem;
+   }
+
+   @media(min-width: 426px) {
     display: flex;
     align-items: center;
     justify-content: center;
-    gap: 2rem;
-    
+    gap: 1rem;
+    margin-bottom: 2rem;
+   }
+
 `
 
 interface CardProps { 
@@ -13,21 +39,61 @@ interface CardProps {
 }
 
 export const Card = styled.div<CardProps>`
-        width: 33rem;
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-        justify-content: center;
-        align-items: flex-end;
-        padding: 1.5rem;
+        
+
         font-size: 1.9rem;
         background: ${props => props.theme['base-project']};
         border-radius: 6px;
 
+        @media(max-width: 425px){
+            padding: 1rem;
+            width: 15rem;
+            display: flex;
+            align-items: flex-end;
+            justify-content: center;
+            flex-direction: column;
+
+        }
+        @media(min-width: 426px) {
+          padding: 1rem;
+          width: 8rem;
+          display: flex;
+          align-items: flex-end;
+          justify-content: center;
+          flex-direction: column;
+          font-size: 1.2rem;
+  }
+  @media(min-width: 768px) {
+          padding: 1rem;
+          width: 15rem;
+          display: flex;
+          align-items: flex-end;
+          justify-content: center;
+          flex-direction: column;
+          font-size: 1.9rem;
+  }
+        @media(min-width:1024px){
+          padding: 1rem;
+          width: 20rem;
+          display: flex;
+          align-items: flex-end;
+          justify-content: center;
+          flex-direction: column;
+        }
+        @media(min-width:1440px){
+          padding: 1rem;
+          width: 25rem;
+          display: flex;
+          align-items: flex-end;
+          justify-content: center;
+          flex-direction: column;
+        }
+        
         & p{
             display: flex;
             align-items: center;
-            gap: 0.5rem;
+            gap: .2rem;
+            
         }
         & svg{
             color: ${props => props.theme['white-100']};
@@ -46,3 +112,4 @@ export const Card = styled.div<CardProps>`
     `}
   
 `
+
